19835d0c0bce0d63502712fa599bf59231af0647
HARSH SABHAYA <harshsabhaya14@gmail.com>
2024-09-15 12:32:02 +0530

    Add files via upload

---
+++ Inverse Alphabet Small Row.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, n;
+
+        printf("enter number of rows : ");
+	scanf("%d", &n);
+
+	for(i=1;i<=n;i++)
+	{
+		for(j=n;j>=i;j--)
+		{
+			printf("%c", i+96);
+		}
+		printf("\n");
+	}
+	return 0;
+}
