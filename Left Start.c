99a84da1fd1ded97f75fc164f1b50377b7f037c3
HARSH SABHAYA <harshsabhaya14@gmail.com>
2024-09-15 12:30:50 +0530

    Add files via upload

---
+++ Left Start.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, k, n;
+        
+        printf("enter number of rows : ");
+	scanf("%d", &n);
+
+	for(i=1;i<=n;i++)
+	{
+		for(k=n;k>i;k--)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
